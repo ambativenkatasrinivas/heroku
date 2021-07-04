02efc76a007c00bb992371f31b28b5d74992e3be
srinivas <ambati.srinivas45@gmail.com>
2021-07-04 11:58:46 +0530

    first deploy

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ app.get('/*', function(req, res) {
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
 app.listen(process.env.PORT || 8080);
 
